35951a194d27f22744a05f7f284426832a77c2dd
*Elizabeth <*elizabethbeaton23@outlook.com*>
2024-12-08 21:05:59 +0000

    Added description of oop

---
+++ python_oop.py
@@ -0,0 +1,16 @@
+"""
+What is object-oriented programming?
+- in order to model our code or anything we'd lik to create wth the code
+- class, object, properties and methods
+eg: class - a categery of something or things, eg a car, human, tree, bird, piano, computer
+and then each class has different objects, this is what makes it unique. eg i the class is human, the object could be 'name'
+as we all have different names, similarily we could have different heights
+a class is a thing we can group different objects in it. and then the object is an instance of this class
+Each object can also have different properties. this can be your first and last name, height, colour of eyes, origin etc. 
+each object has different properties.
+another example: a car could be the name of a class, the object could be a your car and the car of your neightbour for eg, and then properties could
+be colour, brand, year is was bought etc
+each object also has methods - this is what an object can do. 
+eg in the real world a human can eat, sleep, walk, talk etc.
+similarly, a car can drive, be stopped, be parked etc - actions the car can do
+"""
