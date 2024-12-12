8487a002dd7a8e370fe064517db7e104e06891eb
*Elizabeth <*elizabethbeaton23@outlook.com*>
2024-12-12 00:29:22 +0000

    importing classes

---
+++ python_importing_python_class-folder.py/python_importing_python_class.py
@@ -0,0 +1,8 @@
+from dog import Dog
+from cat import Cat
+"""can have multiple files in one import. Similarly can write * to import all Classes"""
+
+lily = Dog("lily")
+print(lily.greet())
+saph = Cat("Saph")
+print(saph.greet())
