c003d52d1c63ada1f7d5279e7df43231dbea4356
*Elizabeth <*elizabethbeaton23@outlook.com*>
2024-12-15 21:08:45 +0000

    included challenge title

--- python_class_inheritance-challenge.py/main.py
+++ python_class_inheritance-challenge.py/main.py
@@ -1,3 +1,11 @@
+"""
+Add a new class inheriting from User called FrenchUser and when greeting, it should say “Bonjour - Name”
+Add a new class inheriting from User called SpanishUser and when greeting, it should say “Hola! - Name”
+Test both classes
+Move each class into their files
+"""
+
+
 from french_user import FrenchUser
 from spanish_user import SpanishUser
 
