861fb47bbcaa293d4496d018eef1f4fbd8dcf694
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-01-09 14:26:23 +0000

    opening csv data file

---
+++ final_project_csv_file.py
@@ -0,0 +1,16 @@
+import csv
+
+#creating variable
+filename = 'WEEK5_PYTHON_FINAL_PROJECT/data.csv' #file name where data is kept. Used outworldindata.com to get data
+
+with open(filename, 'r') as csvfile: # we're only going to read the file as we're creating a graph from it
+    reader = csv.DictReader(csvfile) # making a new variable for pythons DictReader object
+    for line in reader:
+        
+        continent = line['continent'] #creating varibles so quicker to pass through print
+        year = line['year']
+        population = line['population']
+        
+        print(continent)
+        print(year)
+        print(population)
