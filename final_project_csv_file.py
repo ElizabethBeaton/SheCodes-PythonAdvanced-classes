1ac6b2c393b3618420dd408fc445c889b71204df
ElizabethBeaton <125323166+ElizabethBeaton@users.noreply.github.com>
2025-01-10 14:31:41 +0000

    Visualising data from CSV File

--- final_project_csv_file.py
+++ final_project_csv_file.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 #creating variable
 filename = 'WEEK5_PYTHON_FINAL_PROJECT/data.csv' #file name where data is kept. Used outworldindata.com to get data
@@ -14,3 +15,13 @@ with open(filename, 'r') as csvfile: # we're only going to read the file as we'r
         print(continent)
         print(year)
         print(population)
+        
+# Data visuaisation
+plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker="o") #adding
+plt.plot([2000, 2010, 2020, 2030], [200, 300, 500, 800], label="Asia", marker="o")
+plt.title("Internet Population per Continent")
+plt.xlabel("Year")
+plt.ylabel("Internet Users")
+plt.grid()
+plt.legend()
+plt.show()
